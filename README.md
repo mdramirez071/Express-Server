e3e3d0d5f5eeb1432ec3d1ffbe3a71a87ccc7176
mdramirez071 <mdramirez071@gmail.com>
2019-06-18 18:23:38 -0700

    adds final project files

---
+++ README.md
@@ -0,0 +1,11 @@
+This project is called "Express Server" for module NODE101.
+
+The goal of this project was to accomplish the following:
+
+1. Create a new server using the Express framework
+2. Log all incoming requests with the morgan logging library
+3. Serve the Top Spots project from the web server
+4. Serve the Top Spots data from the web server
+5. Update your Top Spots front end with your new back end
+
+Clone the repository locally to your computer. Open up the project in Visual Studio Code. Open up Terminal, run the command "npm start" //This will load the project into the web browser. Once the server is live, go to http://localhost:3000 in your browser. You should see a message displayed on the website that says "New Express Server". Afterwards, go to http://localhost:3000/data to view all of the contents inside of DATA JSON file. Lastly, to bring down the server, simply go to the Terminal and do "Ctrl + C" to take down the localhost server.
--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data');
+//console.log(data); //this console.log verifies that the data.json file is being grabbed properly.
 
+//the line below declares the exact PORT Number that we want to use for our environment.
+//const PORT = process.env.PORT || 3030;
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev')); //morgan is the middleware in this case
+
+
+//this is a route to our New Express Server which returns a status code of 200.
+app.get('/', function(req, res) { 
+    res.status(200).send('New Express Server');
+})
+
+app.get('/data', function(req, res) {
+    res.status(200).send(data);
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('No Page Found');
+});
 
 // finally export the express application
 module.exports = app;
